56f55d20f67acebbef77a33c533cd42ac939e69d
Marcus Grant <marcusfg@pm.me>
2024-11-18 16:15:03 +0100

    Ft: Add ShotcodeSchemaTest for core model

--- core/tests.py
+++ core/tests.py
@@ -1,2 +1,68 @@
 from django.test import TestCase
+from django.db import models
 from unittest.mock import patch
+
+from .models import Shortcode
+from .shortcode import SHORTCODE_MAX_LEN, SHORTCODE_MIN_LEN
+
+
+class ShortcodeSchemaTest(TestCase):
+    def test_shortcode_field_names_types(self):
+        """Test that the Shortcode entity has the expected fields"""
+        # Get actual field instances in lists
+        fields = Shortcode._meta.get_fields()
+        field_names = [field.name for field in fields]
+
+        # Define expected results
+        expected_fields = {
+            "id": models.CharField,
+            "ctype": models.CharField,
+            "url": models.URLField,
+            "btime": models.DateTimeField,
+            "mtime": models.DateTimeField,
+        }
+
+        # Assert that field names are present and have the right types
+        for expect_field_name, expect_field_type in expected_fields.items():
+            with self.subTest(field_name=expect_field_name):
+                self.assertIn(expect_field_name, field_names)
+                field = Shortcode._meta.get_field(expect_field_name)
+                self.assertIsInstance(field, expect_field_type)
+
+    def test_id_field_constraints(self):
+        """Test that the id field has the expected constraints & atrributes"""
+        id_field = Shortcode._meta.get_field("id")
+        self.assertTrue(getattr(id_field, "primary_key", False))
+        self.assertEqual(getattr(id_field, "max_length", None), SHORTCODE_MAX_LEN)
+
+    def test_ctypes_field_constraints(self):
+        """Test that the ctype field has the expected constraints & atrributes"""
+        expect_choices = [
+            ("url", "URL"),
+            ("txt", "Text"),
+            ("pic", "Picture"),
+        ]
+        ctype_field = Shortcode._meta.get_field("ctype")
+        self.assertEqual(getattr(ctype_field, "max_length", None), 3)
+        self.assertEqual(getattr(ctype_field, "choices", None), Shortcode.CONTENT_TYPES)
+        actual_choices = getattr(ctype_field, "choices", [])
+        self.assertCountEqual(actual_choices, expect_choices)
+        for choice in actual_choices:
+            self.assertIn(choice, expect_choices)
+
+    def test_url_field_constraints(self):
+        """Test that the url field has the expected constraints & atrributes"""
+        url_field = Shortcode._meta.get_field("url")
+        self.assertEqual(getattr(url_field, "max_length", None), 192)
+        self.assertTrue(getattr(url_field, "blank", False))
+        self.assertTrue(getattr(url_field, "null", True))
+
+    def test_btime_field_constraints(self):
+        """Test that the btime field has the expected constraints & atrributes"""
+        btime_field = Shortcode._meta.get_field("btime")
+        self.assertTrue(getattr(btime_field, "auto_now_add", False))
+
+    def test_mtime_field_constraints(self):
+        """Test that the mtime field has the expected constraints & atrributes"""
+        mtime_field = Shortcode._meta.get_field("mtime")
+        self.assertTrue(getattr(mtime_field, "auto_now", False))
