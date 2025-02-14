579bbace37f3c1abd954e5beb080dffc321fb231
Marcus Grant <marcusfg@pm.me>
2025-02-14 17:22:56 +0100

    ref/core-tests: move ShortcodeDetailsViewTest
    * ShortcodeDetailsViewTest...
      * Moved to own test module in core.tests.views.test_code_detail
      * Mirrors upcoming structure of core.views.code_detail
    * Removed redundant `WebIndexViewTest` from old tests.
    * Renamed `core/tests.py` to `core/tests_old.py` for legacy tests.
      * This is to prevent name collision with new test supermodule

---
+++ core/tests/views/test_code_detail.py
@@ -0,0 +1,29 @@
+# core/tests/views/test_code_detail.py
+from django.urls import reverse
+from django.test import TestCase
+
+from core.link.models import LinkItem
+
+
+class ShortcodeDetailsViewTest(TestCase):
+    def setUp(self):
+        self.link = LinkItem.ensure("https://google.com")
+
+    # TODO: Need to figure out how to deal with 404
+    # def test_non_existent_shortcode(self):
+    #     """Test failed shortcode lookup renders 404-lookup.html"""
+    #     resp = self.client.get(reverse("shortcode_details", args=["noExist"]))
+    #     self.assertEqual(resp.status_code, 404)
+    #     # Because HttPResponseNotFound cant be tested against template use,
+    #     # Check for a commented out string with a test marker
+    #     self.assertContains(resp, "404-lookup.html", status_code=404)
+    #     self.assertContains(resp, "testMarker", status_code=404)
+
+    def test_valid_shortcode_renders_details(self):
+        """Test valid shortcode form request renders details page content"""
+        shortcode = self.link.item.code
+        resp = self.client.get(reverse("shortcode_details", args=[shortcode]))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "shortcode-details.html")
+        self.assertContains(resp, shortcode)
+        self.assertContains(resp, self.link.url)
--- core/tests_old.py
+++ core/tests_old.py
@@ -233,39 +233,6 @@ class ItemContextTest(TestCase):
 ### View Tests ###
 
 
-class WebIndexViewTest(TestCase):
-    def test_get_request_renders_index(self):
-        """Test root GET request renders index.html"""
-        resp = self.client.get(reverse("web_index"))
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, "index.html")
-        self.assertNotContains(resp, "error")
-        self.assertNotContains(resp, "shortcode is: <strong>")
-
-    def test_root_post_request_creates_item(self):
-        """Test root POST request creates an item"""
-        url = "https://www.google.com"
-        resp = self.client.post(reverse("web_index"), {"content": url})
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, "index.html")
-
-        # Check response contents against Item contents
-        shortcode = hash_b32(url)[:SHORTCODE_MIN_LEN]
-        pattern_href = f'href="[^"]*{shortcode}/details"'
-        self.assertContains(resp, f"{shortcode}</strong>")
-        self.assertRegex(resp.content.decode(), pattern_href)
-
-    # NOTE: I can't think of a case where this will happen
-    # def test_post_no_content(self):
-    #     """Test POST request missing content"""
-    #     # breakpoint()
-    #     resp = self.client.post(reverse("web_index"))
-    #     breakpoint()
-    #     self.assertEqual(resp.status_code, 400)
-    #     self.assertTemplateUsed(resp, "index.html")
-    #     self.assertContains(resp, "Content is required")
-
-
 class ShortcodeDetailsViewTest(TestCase):
     def setUp(self):
         self.link = LinkItem.ensure("https://google.com")
