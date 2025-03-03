8bed37318fb28e10e0c715985098fcb28a918bd0
Marcus Grant <marcusfg@pm.me>
2025-03-03 16:05:45 +0100

    ft/login: Clean core.tests.views.test_login code
    * Remove unused imports
    * Move now to module scoped now_timestamp func.
    * Remove unused module scoped variables.

--- core/tests/views/test_login.py
+++ core/tests/views/test_login.py
@@ -1,16 +1,16 @@
 # core/tests/views/test_login.py
 import jwt
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
 
 from django.conf import settings
 from django.test import TestCase, Client
 from django.urls import reverse
 
 from core.models.user import User
-from core.views.user import JWT_EXP_DELTA_SECONDS
 
-# JWT_EXP_DELTA_SECONDS = 3600
 
+def now_timestamp():
+    return int(datetime.now(timezone.utc).timestamp())
 
 
 class WebLoginViewTests(TestCase):
@@ -33,7 +33,6 @@ class WebLoginViewTests(TestCase):
         self.assertIn('<input type="password', content)
         self.assertIn('<button type="submit"', content)
 
-
     def test_successful_web_login_sets_cookie(self):
         """POST requests: w| valid creds:
         * Redirect to index page with code 302
@@ -44,7 +43,6 @@ class WebLoginViewTests(TestCase):
         resp = self.client.post(self.login_url, data)
         token = resp.cookies["auth_token"].value
         decoded = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-        now = int(datetime.now(timezone.utc).timestamp())
 
         self.assertEqual(resp.status_code, 302)  # Redirect to index w| 302
         self.assertEqual(resp["Location"], self.index_url)
@@ -52,7 +50,7 @@ class WebLoginViewTests(TestCase):
         self.assertTrue(token, "JWT token should not be empty")
         self.assertEqual(decoded["name"], "tester")
         self.assertEqual(decoded["email"], "test@example.com")
-        self.assertGreaterEqual(decoded["exp"], now)
+        self.assertGreaterEqual(decoded["exp"], now_timestamp())
 
     def test_post_login_invalid_creds_returns_401(self):
         """POST with invalid creds should return 401 Unauthorized"""
@@ -85,9 +83,6 @@ class APILoginTests(TestCase):
         self.user.set_password("password")
         self.user.save()
 
-    def now(self):
-        return int(datetime.now(timezone.utc).timestamp())
-
     def test_valid_creds_return_valid_jwt(self):
         """POST to /api/login w| valid creds returns:
         200 code, plain text JWT token in body & X-Auth_token header"""
@@ -102,7 +97,7 @@ class APILoginTests(TestCase):
         self.assertEqual(resp["X-Auth-Token"], token)
         self.assertEqual(decoded["name"], "tester")
         self.assertEqual(decoded["email"], "test@example.com")
-        self.assertGreaterEqual(decoded["exp"], self.now())
+        self.assertGreaterEqual(decoded["exp"], now_timestamp())
 
     def test_non_post_get_request_returns_error(self):
         """If request method is neither POST or GET, return 405 Method Not Allowed"""
