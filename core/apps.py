52d0de638012dcb3ce4cdc2b7c0365344c38acb3
Marcus Grant <marcusfg@pm.me>
2025-03-05 16:38:20 +0100

    ft/login: Fix prepopulation

--- core/apps.py
+++ core/apps.py
@@ -25,6 +25,8 @@ class CoreConfig(AppConfig):
         # Import the User model (assuming it is in core/models/user.py)
         from core.models.user import User
 
+        print("Prepopulating users")
+
         # Get the config file path from settings.
         cfg_file = getattr(settings, "USERS_FILE", None)
         if not cfg_file or not cfg_file.exists():
@@ -32,18 +34,22 @@ class CoreConfig(AppConfig):
         try:
             with open(cfg_file, "r") as f:
                 cfg_users = json.load(f)
-            for cfg_user in cfg_users:
-                # Determine if a user with this email already exists.
-                if not User.objects.filter(email=cfg_user["email"]).exists():
-                    name, email = cfg_user["name"], cfg_user["email"]
-                    user = User.objects.create(email=email, name=name)
-                    user.set_password(cfg_user["password"])
-                    user.save()
         except json.JSONDecodeError as e:
             logger.error(f"JSON decoding error for users file: {e}")
             return
         except FileNotFoundError as e:
             logger.error(f"Error reading users file: {e}")
             return
-        finally:
-            logger.info("User prepopulation complete")
+        for cfg_user in cfg_users:
+            # Determine if a user with this email already exists.
+            if not User.objects.filter(email=cfg_user["email"]).exists():
+                name, email = cfg_user["name"], cfg_user["email"]
+                hashed = make_password(cfg_user["password"])
+                User.objects.create(email=email, name=name, pass_hash=hashed)
+                logger.info(f"User {name} created with email {email}")
+                print(f"User {name} created with email {email}")
+            else:
+                msg = f"User with email {cfg_user['email']} already exists, skipping."
+                logger.info(msg)
+        logger.info("User prepopulation complete")
+        print("User prepopulation complete")
